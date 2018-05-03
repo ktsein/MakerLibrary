591461dcdac01841a1f1fef1bbe0a474f4311d93
Kyaw <kyawtunsein.gla@gmail.com>
2018-05-03 21:44:08 +0100

    updated the website look

--- product.js
+++
@@ -1,9 +0,0 @@
-var elem = document.querySelector('.carousel');
-var instance = M.Carousel.init(elem, options);
-
-// Or with jQuery
-
-$(document).ready(function(){
-  $('.carousel').carousel();
-});
-    
